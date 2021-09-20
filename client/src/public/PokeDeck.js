9ea192252675cfb1edee627af52e973047fc7c7e
unknown <abaziamirr@gmail.com>
2021-09-20 19:48:16 +0200

    pokeDeck update

--- client/src/public/PokeDeck.js
+++ client/src/public/PokeDeck.js
@@ -1,27 +1,35 @@
+import { useState } from "react";
+
 const Poke = () => {
-  
-  let pokemonContent;
-  const createDigits = () => {
-  pokemonContent = [];
-  let randomNuber = Math.floor(Math.random() * (1 + 300));
-  console.log(randomNuber);
-  pokemonContent.push(
-    <div className="img-wraper" key={randomNuber}>
-      <img alt='poke-img'
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/620b6da2dbf05241d1c93ceffac5e6bc6da05e74/sprites/pokemon/other/dream-world/${randomNuber}.svg`}
-      />
-      <p>RandomPoke {randomNuber}</p>
-    </div>
-  );    
-    console.log(pokemonContent);
-  };
 
+    const [getPokemonContent, setPokemonContent] = useState([]);
 
-  return (
-    <div className='pokeWrap'>
-      <button onClick={createDigits}>NEW POKEMON</button>   
-      {pokemonContent}
-    </div>
-  )
+    const createDigits = () => {
+        let randomNuber = Math.floor(Math.random() * (1 + 300));
+        setPokemonContent(
+            [
+                ...getPokemonContent,
+                randomNuber,
+            ],
+        );
+    };
+
+    return (
+        <div className='pokeWrap'>
+            <button onClick={createDigits}>NEW POKEMON</button>
+            {
+                getPokemonContent.map((i) => {
+                    return (
+                        <div className="img-wraper" key={i}>
+                        <img alt='poke-img'
+                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/620b6da2dbf05241d1c93ceffac5e6bc6da05e74/sprites/pokemon/other/dream-world/${i}.svg`}
+                        />
+                        <p>RandomPoke {i}</p>
+                        </div>
+                    );
+                })
+            }
+        </div>
+    )
 };
 export default Poke;
